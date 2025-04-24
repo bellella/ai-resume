cf008ff5644042c94710320b2063d52806a54541
jalmari1 <almariojanallen@gmail.com>
2025-04-24 19:14:47 -0400

    feat: integrate MongoDB for user data handling in Cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from "cypress";
+const { MongoClient } = require('mongodb');
+require('dotenv').config({ path: './apps/api/.env' });
+
 
 export default defineConfig({
   e2e: {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      API_KEY: process.env.API_KEY,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        async findInMongo(query) {
+          const uri = config.env.DATABASE_URL
+          const client = new MongoClient(uri)
+          await client.connect()
+          const db = client.db()
+          const result = await db.collection('User').findOne(query)
+          await client.close()
+          return result
+        },
+      })
     },
   },
 });
--- cypress/e2e/auth.spec.cy.ts
+++ cypress/e2e/auth.spec.cy.ts
@@ -6,6 +6,16 @@ describe('Sign up functionality', () => {
     cy.get('span').contains('Profile').parent().click()
     cy.location("pathname").should("eq", "/login")
     cy.get('.text-sm > .text-primary').click()
-    
+    cy.location("pathname").should("eq", "/signup")
+    cy.get('input[name="name"]').type('Test User')
+    cy.get('input[name="email"]').type('testuser@test.com')
+    cy.get('input[name="password"]').type('Test@1234')
+    cy.get('input[name="confirmPassword"]').type('Test@1234')
+    cy.get('button').contains('Sign Up').click()
+    cy.get('.toast').should('be.visible').and('contain', 'Signup successful!')
+    cy.location("pathname").should("eq", "/login")
+    cy.task('findInMongo', { name: 'Test User' }).then((doc) => {
+      expect(doc).to.have.property('email', 'testuser@test.com')
+    })
   })
 })
